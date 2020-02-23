2bd02ccc1018e209ff17454a60bdaf30340f1544
derek-ye <0derek.ye@gmail.com>
2020-02-22 19:53:41 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools as st;
+
+st.setup(
+  version = "0.0.0",
+  name = "dummy-test",
+  description = "A dummy module to test",
+  keywords = "dummy test",
+  url = "", # github repo url
+  download_url = "",
+)
---
+++ tests/index.py
@@ -0,0 +1,7 @@
+import dummy;
+# import dummy.submodule;
+# from dummy.submodule import ...
+
+dummy.sumNum([1, 2, 3, 4])
+instance = dummy.Dummy()
+instance.printSomething()
